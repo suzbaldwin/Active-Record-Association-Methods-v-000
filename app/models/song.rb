fde5bfbc72709e202bcd116e0890fab94530bd14
suzy baldwin <gbaldwin24@comcast.net>
2018-02-28 16:05:41 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,12 +3,12 @@ class Song < ActiveRecord::Base
   belongs_to :genre
 
   def get_genre_name
-    # self.genre.name
+    self.genre.name
   end
 
   def drake_made_this
 
-    # self.artist = Artist.find_or_create_by(name: "Drake")
+    self.artist = Artist.find_or_create_by(name: "Drake")
 
 
 
